6b86152643dd69b72037257881392693db1cccba
hackliff <xavier.bruhiere@gmail.com>
2013-11-25 19:24:55 +0100

    chore(*): design and fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ PaymentOption.create(
         {
             amount: 5.00,
             amount_display: '$5',
-            description: '<strong>Basic level: </strong>You receive a great big thankyou from us!  You Rock',
+            description: '<strong>Supporter: </strong>You are part of the project and you receive a sincere thank you from us',
             shipping_desc: '',
             delivery_desc: '',
             limit: -1
@@ -19,17 +19,25 @@ PaymentOption.create(
         {
             amount: 100.00,
             amount_display: '$100',
-            description: '<strong>Package 1: </strong>You receive our print edition',
-            shipping_desc: 'add $3 to ship outside the US',
-            delivery_desc: 'Estimated delivery: Oct 2013',
-            limit: 250
+            description: '<strong>Strategic member : </strong>You will be granted with an early access to our private beta and become a pioneer of Unide',
+            shipping_desc: '',
+            delivery_desc: 'Estimated connection: Dec 2013',
+            limit: 50
         },
         {
-            amount: 125.00,
-            amount_display: '$125',
-            description: '<strong>Package 2: </strong>You will receive both our print and digital edition',
-            shipping_desc: 'add $3 to ship outside the US',
-            delivery_desc: 'Estimated delivery: Oct 2013',
+            amount: 1000.00,
+            amount_display: '$1000',
+            description: '<strong>Premium Sponsor : </strong>Get a priority vote on features development, a team access to Unide and priviledged place on our site',
+            shipping_desc: '',
+            delivery_desc: 'Estimated delivery: Mar 2014',
+            limit: 10
+        },
+        {
+            amount: 10000.00,
+            amount_display: '$10000',
+            description: '<strong>Diamond Sponsor : </strong>Receive everything above plus an opportunity to exchange views on the project with our team around a great dinner in Paris',
+            shipping_desc: '',
+            delivery_desc: 'Estimated delivery: Mar 2014',
             limit: -1
         }
     ])
